b2d00375e5fabfffc02dca2371636bb4ecaed17c
Vincent0Lee <vincent0lee95@gmail.com>
2022-01-31 14:19:34 +0800

    handle css file & restructure onload

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -20,22 +20,49 @@ const fileCache = localForage.createInstance({
 export const fetchPlugin = (inputCode: string) => ({
   name: 'fetch-plugin',
   setup(build: esbuild.PluginBuild) {
-    build.onLoad({ filter: /.*/ }, async (args: any) => {
-      // Hijack the esbuild, if it want to search index.js in file system, instead we load them for it.
-      if (args.path === 'index.js') {
-        return {
-          loader: 'jsx',
-          contents: inputCode,
-        };
-      }
+    // 1. Hijack the esbuild, if it want to search index.js in file system, instead we load them for it.
+    build.onLoad({ filter: /(^index\.js$)/ }, () => ({
+      loader: 'jsx',
+      contents: inputCode,
+    }));
 
+    // Extracting Common Caching Logic, since esbuild execute all the onload function one by one.
+    // eslint-disable-next-line consistent-return
+    build.onLoad({ filter: /.*/ }, async (args: any) => {
       const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
         args.path
       );
       if (cachedResult) {
         return cachedResult;
       }
+    });
 
+    // 2. Handling css file
+    build.onLoad({ filter: /.css$/ }, async (args: any) => {
+      const { data, request } = await axios.get(args.path);
+      // encode (into one line) the whatever css file that can be passed into the param
+      const escaped = data
+        .replace(/\n/g, '')
+        .replace(/"/g, '\\"')
+        .replace(/'/g, "\\'");
+      // trick the esbuild to render css
+      const contents = `
+      const style = document.createElement('style');
+      style.innerText = '${escaped}';
+      document.head.appendChild(style)`;
+
+      const result: esbuild.OnLoadResult = {
+        loader: 'jsx',
+        contents,
+        resolveDir: new URL('./', request.responseURL).pathname,
+      };
+      await fileCache.setItem(args.path, result);
+
+      return result;
+    });
+
+    // 3. normal js file
+    build.onLoad({ filter: /.*/ }, async (args: any) => {
       const { data, request } = await axios.get(args.path);
 
       const result: esbuild.OnLoadResult = {
