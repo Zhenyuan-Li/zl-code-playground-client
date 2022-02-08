eed90da4db7656e32e449edea72ae6a0150714b7
Vincent0Lee <vincent0lee95@gmail.com>
2022-02-08 23:11:37 +0800

    optimize preview window & add loading spinner

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,6 +7,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import './code-cell.css';
 
 interface CodeCellProps {
   cell: Cell;
@@ -17,15 +18,22 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
 
+    // eslint-disable-next-line consistent-return
     return () => {
       clearTimeout(timer);
     };
     // If add createBundle directly, there will be a infinite loop: because the use-action hooks
     // (it bind all action and provide a different a action version)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
@@ -43,7 +51,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} bundlingStatus={bundle.err} />}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundlingStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
